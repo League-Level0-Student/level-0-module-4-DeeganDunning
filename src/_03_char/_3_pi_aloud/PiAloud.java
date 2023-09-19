742fcef371a03a3069add69217f84fad3e2b22c2
student <student@192.168.1.98>
2023-09-19 14:00:02 -0700

    t

--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -2,52 +2,73 @@ package _03_char._3_pi_aloud;
 
 import java.util.Scanner;
 
+import game_tools.Sound;
+
 public class PiAloud {
-    /***********  SOUND ***************
-     * Some computers are unable to play sounds.
-     * If you cannot play sound on this computer, set canPlaySounds to false.
-     * If you are not sure, ask your teacher
-     * *****************/
-    boolean canPlaySounds = true;
+	/***********  SOUND ***************
+	 * Some computers are unable to play sounds.
+	 * If you cannot play sound on this computer, set canPlaySounds to false.
+	 * If you are not sure, ask your teacher
+	 * *****************/
+	boolean canPlaySounds = true;
 
 
-    // 1. Make a main method and make sure your program can run
-
-    // 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
-    // 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-    // Check the console shows 3.1
-
-    // 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
-
-    // 5. Skip this step if your computer cannot play sounds.
-    //if (canPlaySounds) {
-        // Use the Sound.speak() method to speak each digit of Pi.
-        
-    //}
+	// 1. Make a main method and make sure your program can run
+	public static void main(String[] args) {
 
 
-    // [CHALLENGE]
-    // *6. Get a character from the user using the getInputFromUser() method
-    // *7. Compare the users' char to the next digit of Pi
-    // *8. If they are correct, print out "correct". If they are not, print "incorrect"
+		// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
+		String pi = "3.141592653589793238";
+		// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+		// Check the console shows 3.1
+	//	System.out.print(pi.charAt(0));
+	//	System.out.print(pi.charAt(1));
+		//System.out.println(pi.charAt(2));
+		// 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
+		for (int i = 0; i < pi.length(); i++ ) {
+		//	System.out.println(pi.charAt(i));
+		}
+		// 5. Skip this step if your computer cannot play sounds.
+		//if (canPlaySounds) {
+		// Use the Sound.speak() method to speak each digit of Pi.
+		// Sound.speak(pi);
+		//}
 
 
-    /********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
+		// [CHALLENGE]
+		// *6. Get a character from the user using the getInputFromUser() method
 
-    private static void pause(int seconds) {
-        try {
-            Thread.sleep(1000 * seconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+		// *7. Compare the users' char to the next digit of Pi
 
-    static char getInputFromUser() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.next().toCharArray()[0];
-    }
+		// *8. If they are correct, print out "correct". If they are not, print "incorrect"
+		for (int i = 0; i < pi.length(); i++ ) {
+			char answer = getInputFromUser(); 
+			
 
-}
+			if (answer == pi.charAt(i)) {
+				System.out.println("Correct");
+			}
+			else { 
+				System.out.println("Incorrect"); 
+				break;
+			}
+		}
+	}
+		/********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
+
+		private static void pause(int seconds) {
+			try {
+				Thread.sleep(1000 * seconds);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		static char getInputFromUser() {
+			Scanner scanner = new Scanner(System.in);
+			return scanner.next().toCharArray()[0];
+		}
+
+	}
 
 
