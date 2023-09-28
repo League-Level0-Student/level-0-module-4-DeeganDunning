b897740f01e42fa7f5ee30c880336ac69e307973
student <student@192.168.1.98>
2023-09-28 13:58:15 -0700

    9-28-2023

--- src/_99_extra/SimpleCalculator.java
+++ src/_99_extra/SimpleCalculator.java
@@ -5,22 +5,34 @@ import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
+		while (true) {
+			// 1. Get 2 numbers from the user and convert them to integer.
 
-		// 1. Get 2 numbers from the user and convert them to integer.
-		String x = JOptionPane.showInputDialog("Input a number.");
-		String y = JOptionPane.showInputDialog("Input another number");
-		int number1 = Integer.parseInt(x);
-		int number2 = Integer.parseInt(y);
-		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply", "Divide" },
-				null);
+			String x = JOptionPane.showInputDialog("Input a number.");
+			String y = JOptionPane.showInputDialog("Input another number");
+			int number1 = Integer.parseInt(x);
+			int number2 = Integer.parseInt(y);
+			// 2. Customize pop-up to support add/subtract/multiply/divide operations.
+			int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
+					JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply", "Divide" },
+					null);
 
-		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-if (operation ==0) {
-	add(number1, number2);
-}
-		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
+			// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
+			if (operation == 0) {
+				add(number1, number2);
+			}
+			if (operation == 1) {
+				subtract(number1, number2);
+			}
+			if (operation == 2 ) {
+				multiply(number1, number2);
+			}
+			if (operation == 3 ) {
+				divide(number1, number2);
+			}
+			// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
+
+		}
 	}
 
 	// 3. Create method for addition operation.
@@ -32,7 +44,7 @@ if (operation ==0) {
 		JOptionPane.showMessageDialog(null, solution);
 	}
 	// 4. Create similar methods for subtraction, multiplication and division.
-	static void substract(int number1, int number2) {
+	static void subtract(int number1, int number2) {
 		int answer2 = number1 - number2;
 		String solution = number1 + " - " + number2 + " = " + answer2;
 		JOptionPane.showMessageDialog(null, solution);
